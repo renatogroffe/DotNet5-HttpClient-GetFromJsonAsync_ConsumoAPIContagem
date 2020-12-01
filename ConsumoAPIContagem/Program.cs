0a79af80615bb57148e53b6744eab7b8746ba876
Renato Groffe <renato.groffe@yahoo.com.br>
2020-12-01 16:35:38 -0300

    .NET 5 + Console Application + HttpClient + Uso do método GetFromJsonAsync + Consumo de API REST de Contagem de Acessos
    .NET 5 + Console Application + HttpClient + Uso do método GetFromJsonAsync + Consumo de API REST de Contagem de Acessos

---
+++ ConsumoAPIContagem/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace ConsumoAPIContagem
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string urlApiContagem = "http://localhost:6001/contador";
+            var client = new HttpClient();
+
+            while (true)
+            {
+                ResultadoContador resultado =
+                    await client.GetFromJsonAsync<ResultadoContador>(urlApiContagem);
+                Console.WriteLine($"Valor Atual do Contador: {resultado.ValorAtual} | " +
+                    $"Local: {resultado.Local} | " +
+                    $"Kernel: {resultado.Kernel} | " +
+                    $"Target Framework: {resultado.TargetFramework} | " +
+                    $"Mensagem Fixa: {resultado.MensagemFixa} | " +
+                    $"Mensagem Variável: {resultado.MensagemVariavel}");
+
+                Console.WriteLine("Pressione CTRL + C para encerrar ou " +
+                    "qualquer outra tecla para continuar...\n");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ ConsumoAPIContagem/ResultadoContador.cs
@@ -0,0 +1,12 @@
+namespace ConsumoAPIContagem
+{
+    public class ResultadoContador
+    {
+        public int ValorAtual { get; set; } 
+        public string Local { get; set; } 
+        public string Kernel { get; set; } 
+        public string TargetFramework { get; set; } 
+        public string MensagemFixa { get; set; } 
+        public string MensagemVariavel { get; set; } 
+    }
+}
